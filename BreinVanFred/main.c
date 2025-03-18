9d4729b04ae624feb06035011601a17e8275d05f
BIG-ERG <alec.lavigne.lux@gmail.com>
2025-03-18 18:44:04 +0100

    minor bug fix

--- BreinVanFred/main.c
+++ BreinVanFred/main.c
@@ -2,11 +2,10 @@
  */
 
 #include <avr/io.h>
+#include "main.h"
 
 int main(void)
 {
-
-    // Insert code
 
     while(1)
     ;
--- BreinVanFred/main.h
+++ BreinVanFred/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-
+#include "IR.h"
+#include "stepperDriver.h"
+#include "ultrasoon.h"
 
 #endif // MAIN_H_INCLUDED
